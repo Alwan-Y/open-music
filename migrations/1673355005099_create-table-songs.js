dc3e99bf7d7af2e3c21551a79e2a8882c5988484
Alwan Yusuf <alwanyusuf19@gmail.com>
2023-01-10 21:32:55 +0700

    checkpoint songs

--- migrations/1673355005099_create-table-songs.js
+++ migrations/1673355005099_create-table-songs.js
@@ -3,19 +3,35 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-    pgm.createTable('albums', {
+    pgm.createTable('songs', {
         id: {
             type: 'VARCHAR(50)',
             primaryKey: true,
             notNull: true,
         },
-        name: {
+        title: {
             type: 'TEXT',
             notNull: true,
         },
         year: {
             type: 'INTEGER',
             notNull: true,
+        },
+        genre: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        performer: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        duration: {
+            type: 'INTEGER',
+            notNull: false,
+        },
+        album_id: {
+            type: 'TEXT',
+            notNull: false,
         },
         created_at: {
             type: 'timestamp',
@@ -31,5 +47,5 @@ exports.up = (pgm) => {
 };
 
 exports.down = (pgm) => {
-    pgm.dropTable('albums');
+    pgm.dropTable('songs');
 };
--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ const Hapi = require('@hapi/hapi');
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumValidator = require('./validator/albums');
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongValidator = require('./validator/songs');
 
 const init = async () => {
     const albumsService = new AlbumsService();
+    const songsService = new SongsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -18,13 +22,22 @@ const init = async () => {
         },
     });
 
-    await server.register({
-        plugin: albums,
-        options: {
-            service: albumsService,
-            validator: AlbumValidator,
+    await server.register([
+        {
+            plugin: albums,
+            options: {
+                service: albumsService,
+                validator: AlbumValidator,
+            },
         },
-    });
+        {
+            plugin: songs,
+            options: {
+                service: songsService,
+                validator: SongValidator,
+            },
+        },
+    ]);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
